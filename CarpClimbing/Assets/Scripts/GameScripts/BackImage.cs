c4e6647dbdb39eef93b96185f9315980e0de3957
udonac3x5s <aa55223315@yahoo.co.jp>
2024-08-09 14:47:41 +0900

    フォルダの追加等

--- CarpClimbing/Assets/Scripts/GameScripts/BackImage.cs
+++ CarpClimbing/Assets/Scripts/GameScripts/BackImage.cs
@@ -18,7 +18,7 @@ public class BackImage : MonoBehaviour
         transform.position += new Vector3(0, -moveSpeed * Time.deltaTime, 0);
 
         // ‹«ŠEü‚ğ’´‚¦‚½‚ç
-        if (transform.position.y < -10.003202f)
+        if (transform.position.y < -8.687283f)
         {
             transform.position = new Vector3(0, 11.31912f, 0);
         }
