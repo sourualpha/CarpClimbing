f92ef7e25df8663b1d2aa2e7d05224cb5d59bcce
GM2140050\ydauser01 <hoshiyama.y2005@gmail.com>
2024-08-11 09:11:12 +0900

    Titleアニメーションの作成中

--- CarpClimbing/Assets/Scripts/TitleManager.cs
+++ CarpClimbing/Assets/Scripts/TitleManager.cs
@@ -5,8 +5,15 @@ using UnityEngine.SceneManagement;
 
 public class TitleManager : MonoBehaviour
 {
+    [SerializeField] GameManager gameManager;
+
+    public float moveSpeed = 0.5f;
     public void PushStartButton()
     {
+        if (0f < gameManager.GameTime)
+        {
+            transform.position = new Vector3(0, 10f, 0);
+        }
         SceneManager.LoadScene("GameScene");
     }
     // Start is called before the first frame update
