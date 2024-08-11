08af451a085c80367de528744f481c0c8ca5defc
GM2140050\ydauser01 <hoshiyama.y2005@gmail.com>
2024-08-11 12:59:05 +0900

    TitleAudio制作

--- CarpClimbing/Assets/Scripts/Title/TitleManager.cs
+++ CarpClimbing/Assets/Scripts/Title/TitleManager.cs
@@ -2,29 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Animations;
 
 public class TitleManager : MonoBehaviour
 {
-    [SerializeField] GameManager gameManager;
-
+    Animator anim;
     public float moveSpeed = 0.5f;
     public void PushStartButton()
     {
-        if (0f < gameManager.GameTime)
-        {
-            transform.position = new Vector3(0, 10f, 0);
-        }
+        //Bool型のパラメーターであるAnimationSwitchをTrueにする
+      //  anim.SetBool("AnimationSwitch", true);
         SceneManager.LoadScene("GameScene");
     }
     // Start is called before the first frame update
     void Start()
     {
-        
+       // anim = GetComponent<Animator>();
+        GetComponent<AudioSource>().Play();
     }
 
     // Update is called once per frame
     void Update()
     {
+
+            //Bool型のパラメーターであるblRotをTrueにする
+            
         
     }
 }
