82cd5714bea61af2d4e787bceec8e6a62f630543
saitoshintaro <shintarosaito0617@gmail.com>
2024-08-11 14:18:55 +0900

    シーン遷移時のアニメーションや効果音追加

--- CarpClimbing/Assets/Scripts/Title/TitleManager.cs
+++ CarpClimbing/Assets/Scripts/Title/TitleManager.cs
@@ -7,26 +7,43 @@ using UnityEngine.Animations;
 public class TitleManager : MonoBehaviour
 {
     Animator anim;
+    AudioSource audioSource;
+
+    [SerializeField]
+    AudioClip SE;
+    [SerializeField]
+    GameObject BackGround;
     public float moveSpeed = 0.5f;
-    public void PushStartButton()
-    {
-        //Bool型のパラメーターであるAnimationSwitchをTrueにする
-      //  anim.SetBool("AnimationSwitch", true);
-        SceneManager.LoadScene("GameScene");
-    }
+
     // Start is called before the first frame update
     void Start()
     {
        // anim = GetComponent<Animator>();
-        GetComponent<AudioSource>().Play();
+        audioSource = GetComponent<AudioSource>();
+        BackGround.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
 
-            //Bool型のパラメーターであるblRotをTrueにする
             
         
+    } 
+    
+    public void PushStartButton()
+    {
+        //Bool型のパラメーターであるAnimationSwitchをTrueにする
+        //  anim.SetBool("AnimationSwitch", true);
+        BackGround.SetActive(true);
+        audioSource.PlayOneShot(SE);
+        StartCoroutine(ChangeScene());
+    }
+
+    IEnumerator ChangeScene()
+    {
+        yield return new WaitForSeconds(1.0f);
+        SceneManager.LoadScene("GameScene");
+
     }
 }
